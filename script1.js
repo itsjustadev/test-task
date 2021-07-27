ab40e6b7b6a23f779c129a735fb647d6593df32c
itsjustadev <itsjustfortwits@gmail.com>
2021-07-27 14:51:58 +0300

    ready

---
+++ script1.js
@@ -0,0 +1,28 @@
+//solution pf (marks are +- equal)
+//we have a mass of marks
+let mass = [5,5,5,5,5,5,5,5];
+let k = 0;
+
+for (let i = 0; i < mass.length; i++) {
+    if (Math.abs(mass[i - 1] - mass[i]) < 2) {
+    k++;
+    }
+}
+if (k >= 3) {
+    console.log('warning');
+}
+
+//solution of (strange time of interview)
+//we have mass of time
+let newTime = 1;
+let mass = [12,2,44,12,56,7];
+let counter = 0;
+
+for (let i = 0; i < mass.length; i++) {
+if (newTime < mass[i]) {
+    counter++;
+}
+}
+if (counter == mass.length || counter > mass.length - 2) {
+  console.log('warning'); 
+}
---
+++ script2.js
@@ -0,0 +1,89 @@
+let mass = [2, 19, 3, 8, 8, 8, 56, 17];
+let given = [];
+let rank = [];
+let k = 0;
+let reserve = 0;
+
+//Sorting
+for (let i = 0; i < mass.length; i++) {
+  
+  given[i] = mass[i];
+    
+};
+
+while (k + 1 < mass.length) {
+  
+  for (let i = k + 1; i < mass.length; i++) {
+  
+  if (mass[i] < mass[k]) {
+    reserve = mass[k];
+    mass[k] = mass[i];
+    mass[i] = reserve;
+  }
+}
+k++;
+}
+//endSorting
+
+console.log(mass);
+
+//countEquals
+
+let count = 0;
+let sign = 0;
+for (let i = 0; i < mass.length; i++) {
+  
+  if (mass[i] == mass[i + 1]) {
+    count++;
+    sign = i + 1;
+  }
+   
+}
+console.log(count);
+console.log(sign);
+
+//endCountEquals: count + 1
+
+//rank
+
+for (let i = 0; i < mass.length; i++) {
+  rank[i] = i + 1; 
+}
+
+console.log(rank)
+
+let sum = 0;
+let number = count;
+while (count >= 0) {
+   sum += rank[sign - count];
+   count--;
+}
+sum = sum / (number + 1);
+
+while (number >= 0) {
+  rank[sign - number] = sum;
+  number--;
+}
+console.log(rank)
+
+//endRank
+
+//objectWork
+let final = [];
+let object = {
+ 2: 1,
+ 3: 2,
+ 8: 4,
+ 17: 6,
+ 19: 7,
+ 56: 8,
+  
+}
+
+
+for (let i = 0; i < given.length; i++) {
+  
+  final.push(object[given[i]]);
+}
+console.log(final);
+final;
